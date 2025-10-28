768071d4de61d4321bc413461bddda9de1ba5a7d
Gdonzeau <gdonzeau@hotmail.com>
2025-10-28 23:28:31 +0100

    Creation of a new project. Waiting for instructions.

---
+++ Test_AMO/Test_AMO/Test_AMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AMOApp.swift
+//  Test_AMO
+//
+//  Created by Guillaume on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test_AMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
